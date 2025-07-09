080701bfa68d4923d4b09dc838cfc2ac258197df
段旭东 <duanxudong@MacBook-Pro.local>
2025-07-09 18:26:27 +0800

    Site updated: 2025-07-09 18:26:26

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
-self.addEventListener("install",e=>{e.waitUntil(caches.open("static-cache").then(e=>e.addAll(["/","/index.html","/styles.css","/images/heycsm-logo32.png","/images/heycsm-logo192.png","/images/heycsm-logo512.png"])))}),self.addEventListener("fetch",s=>{s.respondWith(caches.match(s.request).then(e=>e||fetch(s.request)))});
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('static-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/styles.css',
+          '/images/heycsm-logo32.png',
+          '/images/heycsm-logo192.png',
+          '/images/heycsm-logo512.png'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
